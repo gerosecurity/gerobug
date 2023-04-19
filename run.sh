19377e40b8756bc018d480fd5b772dc7cda43233
Billy Sudarsono <billysudarsono@Billy-Sudarsono.local>
2023-04-19 20:37:27 +0700

    Fix folders

--- run.sh
+++ run.sh
@@ -4,17 +4,17 @@ echo "IP Address: " $(ip addr | grep inet)
 
 echo "=============================="
 
-if [ ! -f ./gerobug_host ]; then
+if [ ! -f ./gerobug_dashboard/gerobug_host ]; then
     rx='([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
     while [[ ! $IP =~ ^$rx\.$rx\.$rx\.$rx$ ]]; do
         echo "Provide Static IP for Gerobug Dashboard (Internal Static IP):"
         read IP
         echo
     done
-    echo $IP > ./gerobug_host
+    echo $IP > ./gerobug_dashboard/gerobug_host
 else
-    echo "Current Allowed Host: $(<./gerobug_host)"
-    echo "[* IS NOT RECOMMENDED, Change to Static Internal IP]"
+    echo "Current Allowed Host: $(<./gerobug_dashboard/gerobug_host)"
+    echo "[* IS NOT RECOMMENDED, CHANGE TO STATIC INTERNAL IP]"
     ANSWER=""
 
     select RESULT in 'Change IP' 'Continue with Current Allowed Host'; do
@@ -34,27 +34,29 @@ else
             read IP
             echo
         done
-        echo $IP > ./gerobug_host
+        echo $IP > ./gerobug_dashboard/gerobug_host
     fi
 fi
 
 echo "=============================="
 
-if [ ! -d ./secrets ]; then
+if [ ! -d ./gerobug_dashboard/secrets ]; then
     echo '[LOG] Creating New Secret Folder...'
-    mkdir secrets
+    mkdir ./gerobug_dashboard/secrets
+    mkdir ./gerobug_web/secrets
 fi
 
-if [ ! -f ./secrets/db_secret.env ]; then
+if [ ! -f ./gerobug_dashboard/secrets/db_secret.env ]; then
     echo '[LOG] Creating New DB Secret...'
-    echo 'POSTGRES_PASSWORD="'$(tr -dc 'A-Za-z0-9!#$%&*?@' </dev/urandom | head -c 30)'"' > ./secrets/db_secret.env    
+    echo 'POSTGRES_PASSWORD="'$(tr -dc 'A-Za-z0-9!#$%&*?@' </dev/urandom | head -c 30)'"' > ./gerobug_dashboard/secrets/db_secret.env 
+    cp  ./gerobug_dashboard/secrets/db_secret.env ./gerobug_web/secrets/db_secret.env 
 fi
 
-if [ ! -f ./secrets/gerobug_secret.env ]; then
+if [ ! -f ./gerobug_dashboard/secrets/gerobug_secret.env ]; then
     echo '[LOG] Creating New Gerobug Secret...'
     DJANGO_SUPERUSER_PASSWORD=$(tr -dc 'A-Za-z0-9!#$%&*?@' </dev/urandom | head -c 30)
     export DJANGO_SUPERUSER_PASSWORD
-    echo 'DJANGO_SUPERUSER_PASSWORD="'$DJANGO_SUPERUSER_PASSWORD'"' > ./secrets/gerobug_secret.env
+    echo 'DJANGO_SUPERUSER_PASSWORD="'$DJANGO_SUPERUSER_PASSWORD'"' > ./gerobug_dashboard/secrets/gerobug_secret.env
 fi
 
 echo "=============================="
