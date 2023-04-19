10dd678f4b0485dcb5e38f297bbc9b485cf74d51
Billy Sudarsono <billysudarsono@Billy-Sudarsono.local>
2023-04-19 09:47:05 +0700

    Improve

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-echo 'POSTGRES_PASSWORD="'$(tr -dc 'A-Za-z0-9!#$%&*?@' </dev/urandom | head -c 30)'"' >> db_secret.env
+#!/bin/bash
+
+if [ ! -f ./db_secret.env ]; then
+    echo 'POSTGRES_PASSWORD="'$(tr -dc 'A-Za-z0-9!#$%&*?@' </dev/urandom | head -c 30)'"' > db_secret.env    
+fi
+
 docker-compose up
