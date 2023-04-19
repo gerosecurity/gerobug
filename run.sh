0d398db23d0b03f698a6a7b195a659caaf6e4ef3
Billy Sudarsono <billysudarsono@Billy-Sudarsono.local>
2023-04-19 13:42:41 +0700

    test

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 if [ ! -f ./secrets/db_secret.env ]; then
     echo '[INFO] Creating DB Secret...'
-    echo 'POSTGRES_PASSWORD="'$(tr -dc 'A-Za-z0-9!#$%&*?@' </dev/urandom | head -c 30)'"' > secrets/db_secret.env    
+    echo 'POSTGRES_PASSWORD="'$(tr -dc 'A-Za-z0-9!#$%&*?@' </dev/urandom | head -c 30)'"' > ./secrets/db_secret.env    
 fi
 
-if [ ! -f ./secrets/gerobug_secret ]; then
+if [ ! -f ./secrets/gerobug_secret.env ]; then
     echo '[INFO] Creating Dashboard Secret...'
     DJANGO_SUPERUSER_PASSWORD=$(tr -dc 'A-Za-z0-9!#$%&*?@' </dev/urandom | head -c 30)
-    echo 'DJANGO_SUPERUSER_PASSWORD='$DJANGO_SUPERUSER_PASSWORD'"' > gerobug_secret.env
     export DJANGO_SUPERUSER_PASSWORD
+    echo 'DJANGO_SUPERUSER_PASSWORD='$DJANGO_SUPERUSER_PASSWORD'"' > ./secrets/gerobug_secret.env
 fi
 
 docker-compose up
