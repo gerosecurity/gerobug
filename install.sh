b88c278386dc9d4a894dbefaf0b7642081dce71d
Billy Sudarsono <billysudarsono@Billy-Sudarsono.local>
2023-04-19 14:37:20 +0700

    installation

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if [ ! -f ./secrets/gerobug_secret.env ]; then
     echo '[LOG] Creating Dashboard Secret...'
     DJANGO_SUPERUSER_PASSWORD=$(tr -dc 'A-Za-z0-9!#$%&*?@' </dev/urandom | head -c 30)
     export DJANGO_SUPERUSER_PASSWORD
-    echo 'DJANGO_SUPERUSER_PASSWORD='$DJANGO_SUPERUSER_PASSWORD'"' > ./secrets/gerobug_secret.env
+    echo 'DJANGO_SUPERUSER_PASSWORD="'$DJANGO_SUPERUSER_PASSWORD'"' > ./secrets/gerobug_secret.env
 fi
 
 docker-compose up
