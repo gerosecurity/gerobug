a753ec70b19be4438c90251eb85dd44bbb05e311
Billy Sudarsono <billysudarsono@Billy-Sudarsono.local>
2023-07-27 17:38:04 +0700

    Gunicorn Nginx - Test 16

--- gerobug_dashboard/gerobug/wsgi.py
+++ gerobug_dashboard/gerobug/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os, logging, gerocert.gerocert
+import os
 
 from django.core.wsgi import get_wsgi_application
 
