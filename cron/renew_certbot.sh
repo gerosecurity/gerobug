87e045fcd8ac330c1ad510557ca6a80047d5ac7f
VGR6479 <billsudarsono@gmail.com>
2024-11-20 10:34:14 +0700

    Auto Renew SSL Script

---
+++ cron/renew_certbot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run --rm -p 8888:80 --name certbot \
+  -v "/etc/letsencrypt:/etc/letsencrypt" \
+  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+  -v "/var/www/letsencrypt:/var/www/letsencrypt" \
+  certbot/certbot renew --webroot -w /var/www/letsencrypt --quiet --agree-tos
+
+# Reload Nginx to apply new certificates (optional)
+# docker exec gerobug_nginx nginx -s reload
+
+# CRON
+# 0 2 1 * * /path/to/gerobug/cron/renew_certbot.sh >> /gerobug/log/renew_certbot.log 2>&1
