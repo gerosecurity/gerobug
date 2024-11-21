bbe90d30ae80c6909f76874b8ccd30b9e5c7209d
Gerobug Team <support@gerobug.com>
2024-11-21 12:32:31 +0800

    Improve Certbot

--- cron/renew_certbot.sh
+++ cron/renew_certbot.sh
@@ -4,7 +4,7 @@ docker run --rm -p 8888:80 --name certbot \
   -v "/etc/letsencrypt:/etc/letsencrypt" \
   -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
   -v "/var/www/letsencrypt:/var/www/letsencrypt" \
-  certbot/certbot renew --webroot -w /var/www/letsencrypt --quiet --agree-tos
+  certbot/certbot renew --webroot -w /var/www/letsencrypt --agree-tos
 
 # Reload Nginx to apply new certificates (optional)
 # docker exec gerobug_nginx nginx -s reload
