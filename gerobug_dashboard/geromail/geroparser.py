9e157fbfa0cf9bfba50e0b4460fddccb1bb4f456
VGR6479 <vgr6479@gmail.com>
2025-10-13 14:21:00 +0200

    Add Error Logging

--- gerobug_dashboard/geromail/geroparser.py
+++ gerobug_dashboard/geromail/geroparser.py
@@ -562,7 +562,6 @@ def company_action(id, note, code):
         hunter_email = report.hunter_email
         hunter = BugHunter.objects.get(hunter_email=hunter_email)
 
-        logging.getLogger("Gerologger").info('[CODE 704] Send Certificate and Bounty Proof to Bug Hunter')
         try:
             gerocert.gerocert.generate(id, hunter.hunter_username, int(report.report_severity))
 
@@ -573,6 +572,8 @@ def company_action(id, note, code):
             # UPDATE REPORT STATUS TO COMPLETE
             report.report_status += 1
             report.save()
+            
+            logging.getLogger("Gerologger").info('[CODE 704] Send Certificate and Bounty Proof to Bug Hunter')
 
         except Exception as e:
             logging.getLogger("Gerologger").error('[ERROR 704] Failed to Complete Report: '+str(e))
